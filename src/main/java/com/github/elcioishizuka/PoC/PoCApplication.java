0bba006e03e98847cbdc7cafd7874c8bc5d1dce2
Elcio <elcio.ishizuka@gmail.com>
2021-12-06 21:04:40 -0300

    Initial commit

---
+++ src/main/java/com/github/elcioishizuka/PoC/PoCApplication.java
@@ -0,0 +1,13 @@
+package com.github.elcioishizuka.PoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/elcioishizuka/PoC/PoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elcioishizuka.PoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
